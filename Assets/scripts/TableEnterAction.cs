de30ad6852fa32bbb80796550163d9622b148bde
oldman <445471528@qq.com>
2020-04-16 19:36:46 +0800

    zzb commit

---
+++ Assets/scripts/TableEnterAction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TableEnterAction : MonoBehaviour
+{
+    private GameObject whiteBallPos;
+    private GameObject ballsPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        whiteBallPos = transform.Find("white ball pos").gameObject;
+        ballsPos = transform.Find("balls pos").gameObject;
+
+        whiteBallPos.SetActive(false);
+        ballsPos.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        Collider[] colliders = GetComponentsInChildren<Collider>(false);
+        foreach (var collider in colliders)
+        {
+            collider.enabled = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlaceTable()
+    {
+        GetComponent<Animator>().SetTrigger("tableEnter");
+    }
+
+    public void OnTableScaleEnd()
+    {
+        Debug.Log("OnTableScaleEnd");
+        whiteBallPos.SetActive(true);
+        ballsPos.SetActive(true);
+        Collider[] colliders = GetComponentsInChildren<Collider>(false);
+        foreach (var collider in colliders)
+        {
+            collider.enabled = true;
+        }
+    }
+}
